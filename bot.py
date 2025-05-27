9ba5d2a3bdefe704790ed306df05083c2ff54ce3
MINDHELL <Tgpurpose640@gmail.com>
2025-05-27 12:20:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from config import API_ID, API_HASH, BOT_TOKEN
 from twitter_utils import get_latest_tweet_url
 from downloader import download_twitter_video
+from health_check import start_health_check
 
 bot = Client("twitter_video_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
@@ -23,5 +24,9 @@ async def fetch_video(client, message):
         return await message.reply("Failed to download the video.")
     
     await message.reply_video(video_path)
-    
-bot.run()
+
+
+# 🔰 Run the Bot
+if __name__ == "__main__":
+    threading.Thread(target=start_health_check, daemon=True).start()
+    bot.run()
